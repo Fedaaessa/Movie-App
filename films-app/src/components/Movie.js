a0532ad7c62e80a799f45094976c2d0b7196cf8a
Fedaa <fedaaessakhalaf@gmail.com>
2021-09-18 21:53:59 +0300

    films-project

---
+++ films-app/src/components/Movie.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const IMG_API  ="https://image.tmdb.org/t/p/w1280";
+
+const setVoteClass=(vote)=>{
+    if(vote >=8){
+        return 'green';
+    } else if (vote >= 6){
+        return 'yellow';
+    }else{
+        return 'red';
+    }
+};
+
+const Movie =({title, poster_path, overview, vote_average})=>(
+<div className="movie">
+    
+<img src={poster_path ? IMG_API + poster_path : "https://images.unsplash.com/photo-1585951237318-9ea5e175b891?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bW92aWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"} alt={title} />
+   <div className="movie-info">
+       <h3 className="movie-title">{title}</h3>
+       <span className={
+           `tag ${setVoteClass(vote_average)}`}
+            >{vote_average}</span>
+   </div>
+<div className="movie-over">
+    <h2>Overview</h2>
+    <p>{overview}</p>
+</div>
+</div>
+);
+
+export default Movie;
